26b803b7c59f60b169e7795cfd7ac33c7836d8eb
Mateus Zitelli <zitellimateus@gmail.com>
2014-01-23 12:54:02 -0200

    Expanded to support session storage too

--- src/storage-utils.js
+++ src/storage-utils.js
@@ -1,6 +1,18 @@
-function DatabaseTable(table_name){
+function DatabaseTable(table_name, storage_type){
+    /*
+    Type must be 'local' or 'session' to define the storage
+    type.
+    */
     if(!table_name || (typeof(table_name) === 'undefined')){
         throw "DatabaseTable(table_name) needs a valid name";
+    }
+    if(!storage_type || (typeof(storage_type) === 'undefined')){
+        this.storage_type = 'local'
+        console.log("DatabaseTable "+table_name+" defined with local storage by default.");
+    }else if(storage_type === 'local' || storage_type === 'session'){
+        this.storage_type = storage_type;
+    }else{
+        throw "The second argument must be 'local' or 'session'";
     }
     this.table_name = table_name; 
     this.parsed_table  = null;
@@ -9,15 +21,32 @@ function DatabaseTable(table_name){
 
 DatabaseTable.prototype.parseDatabaseTable = function(){
     try{
-        this.parsed_table = JSON.parse(localStorage[this.table_name]);
+        this.parsed_table = JSON.parse(this.getFromStorage());
     } catch(err){
         this.parsed_table = [];
     }
     return this.parsed_table;
 };
 
+DatabaseTable.prototype.getFromStorage = function(){
+    if(storage_type === 'local'){
+        localStorage.getItem(this.table_name);
+    }else if(storage_type === 'session'){
+        sessionStorage.getItem(this.table_name);
+    }
+}
+
+DatabaseTable.prototype.setInStorage = function(value){
+    if(storage_type === 'local'){
+        localStorage.setItem(this.table_name, value);
+    }else if(storage_type === 'session'){
+        sessionStorage.setItem(this.table_name, value);
+    }
+}
+
 DatabaseTable.prototype.saveState = function(){
-    localStorage[this.table_name] = JSON.stringify(this.parsed_table);
+    var stringParsedTable = JSON.stringify(this.parsed_table);
+    this.setInStorage(stringParsedTable);
 };
 
 DatabaseTable.prototype.deleteObjectFromPosition = function(position){
